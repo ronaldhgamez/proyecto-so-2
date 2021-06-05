003c7e45b0ab0c07e98a3efe64b4565c4ce3a0ca
ronaldhgamez <ronaldhgamez@gmail.com>
2021-06-05 01:26:50 -0600

    get news from bbc function

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace console_proyecto_so_2
 {
     class Program
     {
+        public static string bbc = "https://www.bbc.com/mundo/topics/c67q9nnn8z7t";
         static void Main(string[] args)
         {
             WebScraping ws = new WebScraping();
-            string text = ws.GetTextFromCRHoy("https://www.crhoy.com/site/dist/portada-tecnologia.php");
+            string text = ws.GetBBCNews(bbc);
             Console.WriteLine(text);
         }
     }
--- WebScraping.cs
+++ WebScraping.cs
@@ -1,6 +1,8 @@
 ﻿using HtmlAgilityPack;
 using ScrapySharp.Extensions;
+using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace console_proyecto_so_2
 {
@@ -13,16 +15,38 @@ namespace console_proyecto_so_2
             this.web = new HtmlWeb();
         }
 
-        /* Get text from CRHoy page */
-        public string GetTextFromCRHoy(string url)
+        /* Get text from BBC home page */
+        public string GetBBCNews(string url)
         {
-            HtmlDocument doc = this.web.Load(url);
-            HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[2]/div/div/span[2]/a[1]");
-            string text = (node == null) ? "name not found" : node.InnerText;
-            return text;
-        }
+            HtmlDocument doc = web.Load(url);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//*[@class=\"lx-stream__post-container\"]");
+            Console.WriteLine("total nodes: " + nodes.Count);
 
-  
+            /* Go through every li html tags */
+            foreach (HtmlNode li_node in nodes)
+            {
+                //Console.WriteLine(li_node.OuterHtml);
+
+                /* get title from news */
+                var html_a = li_node.CssSelect(".qa-heading-link").First();
+                string title = html_a.InnerText;
+
+                /* get paparagraph from news */
+                var html_p = li_node.CssSelect(".lx-stream-related-story--summary").First();
+                string paragraph = html_p.InnerText;
+                
+                /* get image from news */
+                var htmlDiv = li_node.CssSelect(".qa-story-image-link").FirstOrDefault();
+                var html_img = htmlDiv.Element("div").Element("img");
+                string img = html_img.Attributes["src"].Value;
+
+                Console.WriteLine(title);
+                Console.WriteLine(paragraph);
+                Console.WriteLine(img);
+                Console.WriteLine("------------\n");
+            }
+            return "\n";
+        }
 
     }
 }
