8e4188db39524b149471ad6273ba63847599bfbe
ronaldhgamez <ronaldhgamez@gmail.com>
2021-06-05 21:56:33 -0600

    new changes

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@ namespace console_proyecto_so_2
 {
     class Program
     {
-        public static string bbc = "https://www.bbc.com/mundo/topics/c67q9nnn8z7t";
+        //public static string bbc = "https://www.bbc.com/mundo/topics/c67q9nnn8z7t";
+
+
+
         static void Main(string[] args)
         {
             WebScraping ws = new WebScraping();
-            string text = ws.GetBBCNews(bbc);
+            string text = ws.GetBBCNews("https://www.bbc.com/mundo/topics/cpzd498zkxgt");
             Console.WriteLine(text);
         }
     }
